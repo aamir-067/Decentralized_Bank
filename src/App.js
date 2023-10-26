fd4c45380b1932f4da2a6061aaab977937cd8deb
aamir khan <aamirkhan@engineer.com>
2023-10-26 00:42:13 -0700

    project init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h3 className="text-green-300 bg-red-100 p-10">hello</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div >
+        <h2 className='text-4xl font-bold text-red-400 bg-yellow-300'>hello</h2>
+    </div>
+  )
+}
+
+export default Navbar
